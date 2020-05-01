3ba4ab1d9404439624d7d31ce7547d96d26cb376
rain.i <rain.i@kakaoenterprise.com>
2020-05-02 08:32:56 +0900

    downloader commit

--- downloader/cmd/main.go
+++ downloader/cmd/main.go
@@ -1,23 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"github.com/cloudrain21/go-playground/downloader/util"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
-func main() {
-	logger := util.GetInstance()
+var logger = util.GetInstance()
 
+func main() {
 	urlSaver := util.NewFileUrlSaver("urls.txt")
 	err := urlSaver.AcquireUrls()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	download(urlSaver)
-
-	logger.Println("ttttt")
+	logger.Println(urlSaver.GetUrls())
+	download(urlSaver.GetUrls())
 }
 
-func download(urlSaver util.UrlSaver) {
+func download(urls []string) {
+	for _, url := range urls {
+		logger.Printf("start download... : %s\n", url)
+
+		res, err := http.Get(url)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		filename := "image/" + filepath.Base(url)
+
+		f, err := os.Create(filename)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		defer f.Close()
+
+		r := bufio.NewReader(res.Body)
+		len, err := io.Copy(f, r)
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		logger.Printf("end download... : %s len(%d)\n", url, len)
+	}
 }
